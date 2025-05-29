ae98b6216df018b471b89947db1d39fc12143700
Maylton <maylton.araujo@gmail.com>
2025-05-29 17:15:07 -0300

    Código completo sem CSS

--- Gamerule.js
+++ Gamerule.js
@@ -1,14 +1,37 @@
 //gerar numero
-let num = Math.floor(Math.random()*100)+1;
-//definir tentativas
-let c = 7;
-document.getElementById("chance").textContent = c + "chances restantes!";
-//comparar palpite
-function chute(){    
-    let guess = document.getElementById("num").value;
-    if(guess == num){
-        document.getElementById("dica").textContent = "Você acertou!";
-        location.reload();
-    }
 
+let num = parseInt(Math.random()*100)+1;
+const valorNegativo = (valor) => valor <=0;
+
+//definir tentativas
+
+let c = 7;
+window.onload = () => document.getElementById("chance").textContent = "Voce tem " + c + " chances!";
+
+//comparar palpite
+
+function chute(){   
+    let guess = document.getElementById("num").value;
+    if(valorNegativo(guess)){
+        document.getElementById("dica").textContent = "Digite um número acima de 0!";
+        return;
+    }else if(guess == num){
+        document.getElementById("dica").textContent = "Você acertou!";
+        c=7;
+        return;
+    }else if(guess > num){
+        document.getElementById("dica").textContent = "O número secreto é menor";
+    }else{
+        document.getElementById("dica").textContent = "O número secreto é maior";
+    }
+    c--;
+    document.getElementById("chance").textContent = "Lhe restam " + c + " Chances!";
+    if(c==0){
+        document.getElementById("dica").textContent = "Você perdeu! O número secreto era " + num + "!";
+    }
+}
+
+//função reiniciar
+function reload(){
+    location.reload();
 }
