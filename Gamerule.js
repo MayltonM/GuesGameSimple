3d2c99e69cab6ef7aa85381122135ea3572b4a4d
Maylton <maylton.araujo@gmail.com>
2025-05-29 08:31:38 -0300

    correção da inicialização das tentativas

--- Gamerule.js
+++ Gamerule.js
@@ -2,9 +2,9 @@
 let num = Math.floor(Math.random()*100)+1;
 //definir tentativas
 let c = 7;
+document.getElementById("chance").textContent = c + "chances restantes!";
 //comparar palpite
-function chute(){
-    document.getElementById("chance").textContent = c + "chances restantes!";
+function chute(){    
     let guess = document.getElementById("num").value;
     if(guess == num){
         document.getElementById("dica").textContent = "Você acertou!";
